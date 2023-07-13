580fdfae5bafbddbbde8b3138871c22bba8e2ba4
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-07-13 18:59:48 +0700

    Simulasi Data

--- sql/tokopedia-order-history.sql
+++ sql/tokopedia-order-history.sql
@@ -46,3 +46,60 @@ CREATE TABLE order_details
 
 SELECT *
 FROM order_details;
+
+ALTER TABLE orders
+    ADD COLUMN created_at TIMESTAMP NOT NULL;
+
+SELECT *
+FROM orders;
+
+
+INSERT INTO orders(id, created_at, seller_id, seller_name, buyer_id, buyer_name, shipping_name, shipping_address,
+                   shipping_phone,
+                   logistic_id, logistic_name, payment_method_id, payment_method_name, total_quantity, total_weight,
+                   total_product_amount, total_shipping_cost, total_shopping_amount, service_charge, total_amount)
+VALUES ('122', NOW(), 'galery_olahraga_indonesia', 'Galeri Olahraga Indoneia', 'jokomorro', 'Joko Morro',
+        'Budi Nugraha', 'Jalan Raya Xxx, Jakarta, DKI Jakarta, 434343', '089898989',
+        'sicepat', 'Sicepat Gokil', 'debit_online', 'Debit Online', 5, 5360,
+        508500, 60000, 568500, 1000, 569500);
+
+SELECT *
+FROM orders;
+
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight,
+                          product_price, quantity, total_amount)
+VALUES ('1', '122', 'p1', 'Basketball', 2300, 177900, 2, 355800);
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight,
+                          product_price, quantity, total_amount)
+VALUES ('2', '122', 'p2', 'Basketball Warna', 500, 98900, 1, 98900);
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight,
+                          product_price, quantity, total_amount)
+VALUES ('3', '122', 'p3', 'Pentil Pompa Bola', 54, 9900, 1, 9900);
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight,
+                          product_price, quantity, total_amount)
+VALUES ('4', '122', 'p4', 'Pompa Bola', 198, 43900, 1, 43900);
+
+
+SELECT * FROM order_details;
+
+# 2 Query
+
+# Order Id = '122'
+
+SELECT * FROM orders WHERE id = '122';
+SELECT * FROM order_details WHERE order_id = '122';
+
+# 1 Query
+
+SELECT * FROM orders as o join order_details od on o.id = od.order_id WHERE o.id = '122';
+
+SELECT * FROM orders WHERE total_amount > 500000;
+
+
+
+
+
